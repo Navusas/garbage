69dd3dfbe15e99d5577297da83b29ca67247f841
Navusas <D.Giedraitis7@gmail.com>
2023-05-16 06:20:38 +0100

    builder pattern: create maze

--- design-patterns-gang-of-4/creational/02-builder/MazeGame.cs
+++ design-patterns-gang-of-4/creational/02-builder/MazeGame.cs
@@ -1,6 +1,17 @@
+using Common.Maze;
+
 namespace DesignPatterns.Builder;
 
 public class MazeGame
 {
-    
+    public Maze CreateMaze(MazeBuilder builder)
+    {
+        builder.BuildMaze();
+        
+        builder.BuildDoor(1);
+        builder.BuildRoom(2);
+        builder.BuildDoor(1,2);
+        
+        return builder.GetMaze();
+    }
 }
