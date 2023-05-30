9bf3d48665cfccb1ecab4c22ee8f1744507c77fb
Navusas <D.Giedraitis7@gmail.com>
2023-05-30 14:36:04 +0100

    Singleton: Add modern ways in C#

--- design-patterns-gang-of-4/creational/05-singleton/Program.cs
+++ design-patterns-gang-of-4/creational/05-singleton/Program.cs
@@ -5,6 +5,7 @@ using Autofac;
 var maze = MazeFactory.GetInstance();
 
 
+
 // I think these days, in .NET world, you would instead be using AutoFac?
 
 // Like so:
@@ -17,3 +18,30 @@ var container = builder.Build();
 var singleton = container.Resolve<ISingleton>();
 // 3. Call a methd
 singleton.ToString();
+
+
+// C# guarantees, that when you use static constructor, and when it's called, it's thread safe, because C# ensures that it is only executed by one thread.
+// So, you don't need to worry about thread safety.
+class Load
+{
+    private static Load _instance;
+    static Load()
+    {
+        _instance = new Load();
+        // The only gotcha is that you only have 1 chance to initialise it, because if it ever throws inside static constructor,
+        // The C# will never call it again, because it caches the exception, and you will have to restart the application.
+    }
+}
+
+
+// Option 2 would be to use Lazy in C#.
+// Using Lazy<T> is thread safe, and it's also lazy, so it will only be initialised when you call it.
+// And also Lazy ensures that only one person can be running this initialisation code at the time.
+// And when the first person finishes, then the value gets cached, and whenever second person gets unblocked
+// it will just return the cached value.
+class LoadLazy
+{
+    private Lazy<LoadLazy> _instance = new Lazy<LoadLazy>(() => new LoadLazy());
+    public LoadLazy Instance => _instance.Value;
+    
+}
