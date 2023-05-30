ca79bd9105e2e95c6ec9e87f02e2238da77bf140
Navusas <D.Giedraitis7@gmail.com>
2023-05-30 13:17:38 +0100

    Use AUtoFac for singleton pattern

---
+++ design-patterns-gang-of-4/creational/05-singleton/ISingleton.cs
@@ -0,0 +1,9 @@
+namespace DesignPatterns.Creational.Singleton;
+
+public class ISingleton
+{
+    
+}
+public class BombedMazeSingleton : ISingleton {}
+public class EnchantedMazeSingleton : ISingleton {}
+public class DefaultMazeSingleton : ISingleton {}
--- design-patterns-gang-of-4/creational/05-singleton/Program.cs
+++ design-patterns-gang-of-4/creational/05-singleton/Program.cs
@@ -1,6 +1,19 @@
 ﻿using DesignPatterns.Creational.Singleton;
-
-Console.WriteLine("Hello World");
+using Autofac;
 
 // Initialise the singleton instance
 var maze = MazeFactory.GetInstance();
+
+
+// I think these days, in .NET world, you would instead be using AutoFac?
+
+// Like so:
+// 1. Register any singleton you want as interface
+var builder = new ContainerBuilder();
+builder.RegisterType<BombedMazeSingleton>().As<ISingleton>().SingleInstance();
+var container = builder.Build();
+
+// 2. Access it like so in console apps, or in ASP.NET Core, you would inject it into your controller
+var singleton = container.Resolve<ISingleton>();
+// 3. Call a methd
+singleton.ToString();
