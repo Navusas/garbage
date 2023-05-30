f06379713e47b008487cd7aae3112bb725f32269
Navusas <D.Giedraitis7@gmail.com>
2023-05-30 13:51:37 +0100

    Add Foo example in adapter patterN

---
+++ design-patterns-gang-of-4/structural/06-adapter/FooExample.cs
@@ -0,0 +1,75 @@
+namespace DesignPatterns.Structural.Adapter;
+
+public class LibraryA
+{
+    public void Start()
+    {
+        // Do some work
+    }
+
+    public void End()
+    {
+        // Do some work
+    }
+}
+
+public class LibraryB
+{
+    public void Begin()
+    {
+        // Do some work
+    }
+
+    public void Terminate()
+    {
+        // Do some work
+    }
+}
+
+public interface ITargetAdapter
+{
+    void Open();
+    void Close();
+}
+
+public class AdapterA : ITargetAdapter
+{
+    private LibraryA A { get; set; }
+
+    public AdapterA( LibraryA a )
+    {
+        this.A = a;
+    }
+
+    public void Open() { this.A.Start(); }
+    public void Close() { this.A.End(); }
+}
+
+public class AdapterB : ITargetAdapter
+{
+    private LibraryB B { get; set; }
+
+    public AdapterB( LibraryB a )
+    {
+        this.B = a;
+    }
+
+    public void Open() { this.B.Begin(); }
+    public void Close() { this.B.Terminate(); }
+}
+
+public class Client
+{
+    public Client()
+    {
+        ITargetAdapter adapter = new AdapterA( new LibraryA() );
+        adapter.Open();
+        adapter.Close();
+    }
+    public void DoWork( ITargetAdapter adapter )
+    {
+        adapter.Open();
+        // Do some work
+        adapter.Close();
+    }
+}
