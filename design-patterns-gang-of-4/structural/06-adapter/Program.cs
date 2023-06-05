53604e6c014e1f11c90ea5de0eda5f8128dbc32d
Navusas <D.Giedraitis7@gmail.com>
2023-06-05 08:13:24 +0100

    Adapter pattern

--- design-patterns-gang-of-4/structural/06-adapter/Program.cs
+++ design-patterns-gang-of-4/structural/06-adapter/Program.cs
@@ -1,42 +1,76 @@
-﻿ITarget target = new Adapter();
-target.Request();
+﻿// ITarget target = new Adapter();
+// target.Request();
+//
+// /// <summary>
+// /// This is the interface which the client expects.
+// /// </summary>
+// public interface ITarget
+// {
+//     void Request();
+// }
+//
+// /// <summary>
+// /// The adapter class adopts the interface which doesn't match the client's needs,
+// /// and adapts it to the client's needs.
+// /// </summary>
+// public class Adapter : ITarget
+// {
+//     private readonly IAdaptee _adaptee = new Adaptee();
+//     public void Request()
+//     {
+//         Console.WriteLine("Doing some extra work here, to fit the client's needs.");
+//         Console.WriteLine("This work will be done on top of what the Adaptee does.");
+//         
+//         // Possibly do some other work
+//         _adaptee.SpecificRequest();
+//     }
+// }
+//
+// /// <summary>
+// /// The class / interface which doesn't fit the client's needs.
+// /// </summary>
+// public interface IAdaptee
+// {
+//     void SpecificRequest();
+// }
+// public class Adaptee : IAdaptee
+// {
+//     public void SpecificRequest()
+//     {
+//         Console.WriteLine("Called SpecificRequest()");
+//     }
+// }
 
-/// <summary>
-/// This is the interface which the client expects.
-/// </summary>
-public interface ITarget
-{
-    void Request();
-}
+var x = new A();
+var y = new B();
 
-/// <summary>
-/// The adapter class adopts the interface which doesn't match the client's needs,
-/// and adapts it to the client's needs.
-/// </summary>
-public class Adapter : ITarget
+Console.WriteLine("Made");
+Console.WriteLine(B.x);
+class A
 {
-    private readonly IAdaptee _adaptee = new Adaptee();
-    public void Request()
+    static A()
     {
-        Console.WriteLine("Doing some extra work here, to fit the client's needs.");
-        Console.WriteLine("This work will be done on top of what the Adaptee does.");
-        
-        // Possibly do some other work
-        _adaptee.SpecificRequest();
+        Console.WriteLine("A static");
+    }
+
+    public A()
+    {
+        Console.WriteLine("A inst");
     }
 }
 
-/// <summary>
-/// The class / interface which doesn't fit the client's needs.
-/// </summary>
-public interface IAdaptee
+class B
 {
-    void SpecificRequest();
-}
-public class Adaptee : IAdaptee
-{
-    public void SpecificRequest()
+    public static int x = Foo();
+
+    static int Foo()
     {
-        Console.WriteLine("Called SpecificRequest()");
+        Console.WriteLine("B static");
+        return 35;
+    }
+
+    public B()
+    {
+        Console.WriteLine("B inst");
     }
 }
