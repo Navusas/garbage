c62ca637ed80acd10cea01084b44da293451aaf9
Clive Tong <clive.tong@red-gate.com>
2023-06-05 15:27:17 +0100

    Half implemented factory

---
+++ design-patterns-gang-of-4/creational/03-factory-method/MazeFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.FactoryMethod
+{
+    interface IDoor
+    {
+    }
+    interface IWall
+    {
+    }
+    interface IMazeFactory
+    {
+        IDoor MakeDoor();
+        IWall MakeWall();
+    }
+
+    class StandardMazeFactory : IMazeFactory
+    {
+        public IDoor MakeDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWall MakeWall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CountingItemsMazeFactory : IMazeFactory
+    {
+        public IDoor MakeDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWall MakeWall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // But there might need to be behaviours across the factories
+
+    interface ICard
+    {
+        bool PayFor(decimal amount);
+    }
+
+    interface ICardFactory
+    {
+        ICard GetCard();
+    }
+
+    class DebitCardMaker : ICardFactory
+    {
+        public ICard GetCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CreditCardMaker : ICardFactory
+    {
+        public ICard GetCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
