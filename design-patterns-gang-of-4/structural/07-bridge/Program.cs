fa872cb12c3c3ea394de297fb03fd16fffba543b
Clive Tong <clive.tong@red-gate.com>
2023-06-05 15:24:38 +0100

    Add bridge and composite

--- design-patterns-gang-of-4/structural/07-bridge/Program.cs
+++ design-patterns-gang-of-4/structural/07-bridge/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("07-bridge");
+﻿var x = new RefinedAbstraction(new ConcreteB());
+x.Operation();
+
+interface IAbstraction
+{
+    public void Operation();
+}
+
+class RefinedAbstraction : IAbstraction
+{
+    private readonly IImplementor _imp;
+
+    public RefinedAbstraction(IImplementor imp)
+    {
+        _imp = imp;
+    }
+    public void Operation()
+    {
+        _imp.OperationImp();
+    }
+}
+
+internal interface IImplementor
+{
+    public void OperationImp();
+}
+
+class ConcreteA : IImplementor
+{
+    public void OperationImp()
+    {
+        Console.WriteLine("concreteA");
+    }
+}
+
+class ConcreteB : IImplementor
+{
+    public void OperationImp()
+    {
+        Console.WriteLine("concreteB");
+    }
+}
--- design-patterns-gang-of-4/structural/08-composite/Program.cs
+++ design-patterns-gang-of-4/structural/08-composite/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("07-bridge");
+﻿var clive = new Individual("clive");
+var dom = new Individual("Dom");
+
+var meeting = new Collection(new[] { clive, dom });
+
+clive.DoIt();
+dom.DoIt();
+
+meeting.DoIt();
+
+interface IThing
+{
+    void DoIt();
+}
+
+class Individual : IThing
+{
+    private readonly string _name;
+
+    public Individual(string name) => _name = name;
+    
+    public void DoIt()
+    {
+        Console.WriteLine($"Individual {_name}");
+    }
+}
+
+class Collection : IThing
+{
+    private readonly List<IThing> _items;
+
+    public Collection(IEnumerable<IThing> items)
+    {
+        _items = items.ToList();
+    }
+    public void DoIt()
+    {
+        Console.WriteLine("Collection");
+        foreach (var item in _items)
+        {
+            item.DoIt();
+        }
+    }
+}
