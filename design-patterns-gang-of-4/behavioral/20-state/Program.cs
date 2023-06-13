105889d64434717d39c1b883f3eded1e3b6d3732
Clive Tong <clive.tong@red-gate.com>
2023-06-13 09:59:25 +0100

    State pattern

--- design-patterns-gang-of-4/behavioral/20-state/Program.cs
+++ design-patterns-gang-of-4/behavioral/20-state/Program.cs
@@ -1,1 +1,48 @@
-﻿Console.WriteLine("07-bridge");
+﻿var counter = new Counter();
+Console.WriteLine(counter.Adjust());
+Console.WriteLine(counter.Adjust());
+Console.WriteLine(counter.Adjust());
+
+counter.ChangeToDecrementMode();
+Console.WriteLine(counter.Adjust());
+Console.WriteLine(counter.Adjust());
+Console.WriteLine(counter.Adjust());
+
+
+class Counter
+{
+    private int _count = 0;
+    private CounterState _state = new IncrementMode();
+
+    public int Adjust()
+    {
+        return (_count = _state.Adjust(_count));
+    }
+
+    public void ChangeToIncrementMode() =>
+        _state = new IncrementMode();
+    public void ChangeToDecrementMode() =>
+        _state = new DecrementMode();
+
+    abstract class CounterState
+    {
+        public abstract int Adjust(int state);
+    }
+
+    class IncrementMode : CounterState
+    {
+        public override int Adjust(int state)
+        {
+            return state + 1;
+        }
+    }
+
+    class DecrementMode : CounterState
+    {
+        public override int Adjust(int state)
+        {
+            return state - 1;
+        }
+    }
+
+}
