43bb7b951f25269631df51fade087010d8a55243
Clive Tong <clive.tong@red-gate.com>
2023-06-13 09:14:19 +0100

    Memento

--- design-patterns-gang-of-4/behavioral/18-memento/Program.cs
+++ design-patterns-gang-of-4/behavioral/18-memento/Program.cs
@@ -1,1 +1,64 @@
-﻿Console.WriteLine("07-bridge");
+﻿
+var originator = new Originator() as IOriginator;
+
+var state = originator.CreateMemento();
+Console.WriteLine(originator.GetValue());
+
+originator.ChangeValue();
+Console.WriteLine(originator.GetValue());
+
+originator.SetMemento(state);
+Console.WriteLine(originator.GetValue());
+
+
+interface IOriginator
+{
+    int GetValue();
+    void ChangeValue();
+    IMemento CreateMemento();
+    void SetMemento(IMemento memento);
+}
+
+interface IMemento  // Discuss this
+{
+}
+
+class Originator : IOriginator
+{
+    private readonly Random _random = new Random();
+    private int _state;
+
+    public Originator()
+    {
+        _state = _random.Next(55);
+    }
+
+    public int GetValue() => _state;
+
+    public void ChangeValue() => _state = _random.Next(55);
+
+    public IMemento CreateMemento()
+    {
+        return new Memento(_state);
+    }
+
+    public void SetMemento(IMemento memento)
+    {
+        if (memento is Memento ourMemento)
+        {
+            _state = ourMemento._state;
+            return;
+        }
+
+        throw new InvalidOperationException("Wrong type of memento supplied");
+    }
+
+    private class Memento : IMemento
+    {
+        public readonly int _state;
+        public Memento(int state)
+        {
+            _state = state;
+        }
+    }
+}
