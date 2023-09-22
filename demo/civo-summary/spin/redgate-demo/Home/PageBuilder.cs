acddf7d92b3c5cf2c1d1d9408787f4a9454448c1
Navusas <D.Giedraitis7@gmail.com>
2023-09-22 08:10:54 +0100

    Add demo from mac

---
+++ demo/civo-summary/spin/redgate-demo/Home/PageBuilder.cs
@@ -0,0 +1,26 @@
+namespace Microservice;
+
+public class PageBuilder
+{
+    private readonly string _relativePathToPage;
+    private string _body;
+
+    public PageBuilder(string relativePathToPage)
+    {
+        _relativePathToPage = relativePathToPage;
+    }
+
+    public PageBuilder WithBody(string body)
+    {
+        _body = body;
+        return this;
+    }
+
+    public string Build()
+    {
+        var template = File.ReadAllText($"/assets/{_relativePathToPage}");
+
+        return template
+            .Replace(" {{ body }}", _body);
+    }
+}
---
+++ demo/civo-summary/spin/redgate-demo/Home/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using Fermyon.Spin.Sdk;
+using System.Runtime.InteropServices;
+
+namespace Microservice;
+
+public static class Handler
+{
+    [HttpHandler]
+    public static HttpResponse HandleHttpRequest(HttpRequest request)
+        => Router.Handle(request);
+}
+
+
+public static class Router
+{
+    public static HttpResponse Handle(HttpRequest request)
+    {
+        // find which path was hit, and switch on it
+        return request.Url switch
+        {
+            "/" => CurrentTimeResponse.Generate(),
+            "/hello" => new HttpResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                BodyAsString = "Hello from .NET",
+            },
+            "/goodbye" => new HttpResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                BodyAsString = "Goodbye from .NET",
+            },
+            _ => new HttpResponse
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                BodyAsString = "Not found",
+            },
+        };
+    }
+}
+
+public static class CurrentTimeResponse
+{
+    public static HttpResponse Generate()
+    {
+        var indexPage = new PageBuilder("index.html")
+            .WithBody("Welcome Sami!")
+            .Build();
+        
+        return new HttpResponse
+        {
+            Headers = new Dictionary<string, string>
+            {
+                ["Content-Type"] = "text/html",
+            },
+            StatusCode = HttpStatusCode.OK,
+            BodyAsString = indexPage
+            // <p>Current architecture: {RuntimeInformation.OSArchitecture}</p>
+        };
+    }
+}
