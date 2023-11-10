c2da8df6ccc8af5c4c3acc8299900478cb037fe0
Navusas <D.Giedraitis7@gmail.com>
2023-11-10 09:02:20 +0000

    Add Static Abstract Interfaces

--- languages-and-frameworks/csharp/csharp-features/Csharp11/Features/StaticAbstractsInInterfaces.cs
+++ languages-and-frameworks/csharp/csharp-features/Csharp11/Features/StaticAbstractsInInterfaces.cs
@@ -3,21 +3,31 @@ namespace Csharp11;
 
 /*
 Title:          Static Abstract In Interfaces
-Description:    Define the overflow behavior of certain operators (defined by user)
+Description:    Allow interfaces to declare static abstract members
 Link:           https://github.com/dotnet/csharplang/blob/main/proposals/csharp-11.0/static-abstracts-in-interfaces.md
 */
 public class StaticAbstractsInInterfaces
 {
-    /// <summary>
-    /// No way to 
-    /// </summary>
     public void DemonstrateBefore()
     {
-        
+        // Rely on some sort of non-generic static methods / classes 
     }
 
     public void DemonstrateAfter()
     {
-
+        int result = IntAddable.Add(5, 10);
+        Console.WriteLine($"After: {result}");
     }
+}
+
+
+public interface IAddable<T>
+{
+    static abstract T Add(T a, T b);
+}
+
+// Implement the interface in a class
+public class IntAddable : IAddable<int>
+{
+    public static int Add(int a, int b) => a + b;
 }
