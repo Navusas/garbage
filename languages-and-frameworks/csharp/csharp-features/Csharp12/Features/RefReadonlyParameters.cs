3dd653b30e17e3ab1f4f660669a987cc484f4365
Navusas <D.Giedraitis7@gmail.com>
2023-11-10 08:06:37 +0000

    Finalize Ref Readonly

--- languages-and-frameworks/csharp/csharp-features/Csharp12/Features/RefReadonlyParameters.cs
+++ languages-and-frameworks/csharp/csharp-features/Csharp12/Features/RefReadonlyParameters.cs
@@ -6,20 +6,20 @@ Description:    Enforce compiler to prevent a method from modifying the passed r
 Link:           https://github.com/dotnet/csharplang/blob/main/proposals/csharp-12.0/ref-readonly-parameters.md
 */
 
-/// <summary>
-/// 'in' vs 'ref readonly'.
-/// 
-/// 'in' -> used to pass read-only reference to a method. It can be used at both the method & the caller. It indicates that the method should not modify the passed parameter.
-/// 'ref readonly' -> only for method declaration. The api/caller/client doesn't care and thus won't break existing codebases. Using `readonly` helps to ensure immutability.
-/// </summary>
 public class RefReadonlyParams
 {
     private ConsoleWriter consoleWriter = new ("C# 12 - RefReadonlyParams");
 
     /// <summary>
-    /// Prior to C# 12
+    /// Prior to C# 12, you would have to rely on documentation to know if a method is modifying the passed parameter.
+    /// 
+    /// Example:
+    /// This method prints value to the console.
+    /// 
+    /// !!! DO NOT MODIFY THE PASSED VALUE !!!
+    /// 
     /// </summary>
-    public void DemonstrateBefore(in int readOnlyValue) 
+    public void DemonstrateBefore(ref int readOnlyValue) 
     {
         // Passed by reference to avoid copying.
         // The 'in' keyword enforces that this method cannot modify the passed value.
@@ -40,6 +40,26 @@ public class RefReadonlyParams
         // The compiler will now prevent any assignment to readOnlyValue,
         // ensuring that it's truly read-only.
         consoleWriter.Write($"After: Value is {readOnlyValue}");
+
+        // The following line would cause a compile-time error:
+        // readOnlyValue = 10;
+    }
+
+
+    /// <summary>
+    /// In C# 7 they introduced `in` keywoard. 
+    /// 
+    /// It is used to specify that an argument is passed by reference and its intended to be read-only.
+    /// 
+    /// in vs ref readonly.
+    /// in              -> used to pass read-only reference to a method. It can be used at both the method & the caller. It indicates that the method should not modify the passed parameter.
+    /// ref readonly    -> only for method declaration. The api/caller/client doesn't care and thus won't break existing codebases. Using `readonly` helps to ensure immutability.
+    /// </summary>
+    public void BonusRound(in int readOnlyValue) 
+    {
+        // Passed by reference to avoid copying.
+        // The 'in' keyword enforces that this method cannot modify the passed value.
+        consoleWriter.Write($"Before: Value is {readOnlyValue}");
 
         // The following line would cause a compile-time error:
         // readOnlyValue = 10;
--- languages-and-frameworks/csharp/csharp-features/Csharp12/Program.cs
+++ languages-and-frameworks/csharp/csharp-features/Csharp12/Program.cs
@@ -8,7 +8,8 @@ usingAliasType.DemonstrateAfter();
 int value = 42;
 
 var refReadonlyParams = new RefReadonlyParams();
-refReadonlyParams.DemonstrateBefore(in value);
+refReadonlyParams.DemonstrateBefore(ref value);
 refReadonlyParams.DemonstrateAfter(ref value);
+refReadonlyParams.BonusRound(value);
 
 Console.WriteLine("[C# 12]: Program ended");
