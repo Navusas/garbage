6c1d56da0a826e03e97648868d045671f49adca2
Navusas <D.Giedraitis7@gmail.com>
2023-12-05 21:25:36 +0000

    [aoc]. Day 1 part 2 (in progress)

--- advent-of-code/2023/2023/day1.py
+++ advent-of-code/2023/2023/day1.py
@@ -27,8 +27,8 @@
 ##########################################
 ## Implementation
 ##########################################
-def main():
-    with open('2023/day1.input') as f:
+def part1():
+    with open('2023/day1-p1.input') as f:
         lines = f.readlines()
 
     sum = 0
@@ -45,8 +45,89 @@ def main():
         sum += int(answer)
         print(f"Line: {line} - Numbers: {numbers}. Choosing {answer} - Sum: {sum}")
     print(f"Sum: {sum}")
+
+##########################################
+## Task
+##########################################
+# Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+# Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+# two1nine
+# eightwothree
+# abcone2threexyz
+# xtwone3four
+# 4nineeightseven2
+# zoneight234
+# 7pqrstsixteen
+# In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+# What is the sum of all of the calibration values?
+
+##########################################
+## Implementation
+##########################################
+def part2():
+    with open('2023/day1-p2.input') as f:
+        lines = f.readlines()
+
+    # list of strings
+    numbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+    numbers_as_str = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
+    all_numbers = numbers + numbers_as_str
+
+    # lines = [line for line in lines if 5 < len(line) < 10 ]
+    total_sum = 0
+    # Go through each line
+    for line in lines:
+        matrix = []
+        line = line.strip()
+        print(f"Line: {line}")
+        # Look at the numnbers
+        for num in all_numbers:
+
+            # If number (digit or string) is inside string
+            # we will build a little "matrix" of which number appeared where
+            # and then we will look at the index of the number in the line
+            if num in line:
+                number_found = all_numbers[all_numbers.index(num)]
+                index_at = line.index(num)
+
+                if number_found in numbers:
+                    number_found_as_digit = number_found
+                else:
+                    number_found_as_digit = numbers[numbers_as_str.index(number_found)]
+
+                matrix += [ [index_at, number_found_as_digit] ]
+                # print(f"Line: {line} - Found: {number_found} (or {number_found_as_digit}) - Index: {index_at}")
+        
+        # print(f"Matrix: {matrix}")
+        # Sort the matrix by the index
+        matrix.sort(key=lambda x: x[0])
+        # print(f"Sorted Matrix: {matrix}")
+
+        matrix_sum =matrix_to_sum(matrix);
+        print(f"Matrix Sum: {matrix_sum} -- Total Sum: {total_sum}")
+        total_sum += matrix_sum
     
+    print(f"Total Sum: {total_sum}")
+
+
+def matrix_to_sum(matrix):
+    """
+    Kind of like day1 implementation ,but uses a matrix to calculate the sum
+    """
+    sum = 0
+    if len(matrix) > 0:
+        answer = matrix[0][1]
+    if len(matrix) > 1:
+        answer += matrix[-1][1]
+    else:
+        answer += matrix[0][1].__str__()
+    sum += int(answer)
+    return sum
 
 if __name__ == "__main__":
     print("Day 1: Trebuchet?!")
-    main()
+    # part1()
+    part2()
